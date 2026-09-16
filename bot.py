f1d03b2d09c7e77d05169ee840a68effb278af24
Abror03282003 <mamatkarimovabror9@gmail.com>
2026-09-16 20:57:24 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ else:
 
 # 2. Qolgan kutubxona va modullarni import qilish
 from aiogram import Bot, Dispatcher
-from database import init_db
+from database import init_db, init_cache_db
 from handlers import download, admin
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
@@ -30,8 +30,9 @@ if YOUTUBE_COOKIES:
 async def main():
     logging.basicConfig(level=logging.INFO)
     
-    # Ma'lumotlar bazasini asinxron ishga tushirish
+    # Ma'lumotlar bazalarini ishga tushirish
     await init_db()
+    init_cache_db()  # Kesh bazasi qo'shildi
     
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
