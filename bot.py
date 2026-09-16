4f29db9209bd3756d27df21493ae2e33ad47e750
Abror03282003 <mamatkarimovabror9@gmail.com>
2026-09-16 13:24:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,10 +5,8 @@ from aiogram import Bot, Dispatcher
 from database import init_db
 from handlers import download, admin
 
-# Environment orqali token va cookies sozlamasini olish
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-# YouTube cookies sozlamasi (Railway uchun)
 YOUTUBE_COOKIES = os.environ.get("YOUTUBE_COOKIES")
 if YOUTUBE_COOKIES:
     with open("cookies.txt", "w") as f:
@@ -25,7 +23,7 @@ async def main():
 
     # Routerlarni ulash
     dp.include_router(admin.router)
-    dp.include_router(download.router)  # Barcha yuklash, qidiruv va audio/video tanish shu yerda
+    dp.include_router(download.router)
 
     logging.info("Bot muvaffaqiyatli ishga tushdi!")
     await dp.start_polling(bot)
